f494acab1d65173fa4248b6f620a737c2d44df4e
plat15 <tripla1505@gmail.com>
2023-03-30 00:38:19 +0700

    Hoan_Doi_Vi_Tri

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double a = Math.random();
+        double b = Math.random();
+        double temp = 0;
+
+        System.out.println("============================================");
+        System.out.println("Before swapping : a, b = " + a + " , " + b);
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After swapping : a, b = " + a + " , " + b);
+        System.out.println("============================================");
+
     }
 }
